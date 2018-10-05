ca80f97b23ce3503f600724ff5bd509b57829d1e
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-05 17:04:30 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,7 +14,7 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT users.name, users.age, SUM(pledges.amount) FROM users
-  JOION pledges ON users.id = pledges.user_id
+  JOIN pledges ON users.id = pledges.user_id
   GROUP BY users.name"
 end
 
