a78ed6000d5b742d086af3d8fce60097a5bcdad5
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-05 17:32:49 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 FROM projects
 JOIN pledges ON pledges.project_id = projects.id
 GROUP BY projects.title
-WHERE x >= 0"
+HAVING x >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
